bf65bf54e10ca8553206a17907d2438e6bf58898
Edward L. Platt <ed@elplatt.com>
2020-02-05 01:39:01 +0000

    Disiable autocomplete.

--- explore/forms.py
+++ explore/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 
 class CommandForm(forms.Form):
+    command_attrs = {
+        'autofocus': 'autofocus',
+        'autocomplete': 'off',
+    }
     command_text = forms.CharField(
         max_length=512,
         label='>',
-        widget=forms.TextInput(attrs={'autofocus':'autofocus'}))
+        widget=forms.TextInput(attrs=command_attrs
+))
