623a9c18f93cd642c49edff81a411d8a42539ba6
Edward L. Platt <ed@elplatt.com>
2020-02-05 03:26:33 +0000

    Add Area model.

--- explore/models.py
+++ explore/models.py
@@ -1,12 +1,29 @@
+from django.conf import settings
 from django.db import models
-from django.db.models import CharField, DateTimeField
+from django.db.models import CharField, DateTimeField, ForeignKey, TextField
 
 # Create your models here.
 
+class Area(models.Model):
+
+    created_at = DateTimeField(auto_now_add=True)
+    creator = ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    title = CharField(max_length=512)
+    description = TextField()
+
+    def __str__(self):
+        return self.title
+
 class Activity(models.Model):
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = DateTimeField(auto_now_add=True)
     activity_text = CharField(max_length=512)
+    area = ForeignKey(Area, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.activity_text
+
