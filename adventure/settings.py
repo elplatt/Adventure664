4ee42ac14c804847c26df8eecc617e22d5ade73c
Edward L. Platt <ed@elplatt.com>
2020-02-05 17:00:21 +0000

    Change static directory and update css.

--- adventure/settings.py
+++ adventure/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 's')
+
+STATICFILES_DIRS =  [
+    os.path.join(BASE_DIR, 'static')
+]
