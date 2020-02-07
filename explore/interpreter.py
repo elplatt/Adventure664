33bf7c13daa877f26359e012345f9a7a7eccc455
Edward L. Platt <ed@elplatt.com>
2020-02-07 16:03:20 +0000

    Validate titles for new connections.

--- explore/interpreter.py
+++ explore/interpreter.py
@@ -3,6 +3,15 @@ from .models import Activity
 
 class Interpreter(object):
 
+    ALLOWED_CONNECTIONS = [
+        'north',
+        'south',
+        'east',
+        'west',
+        'up',
+        'down'
+    ]
+
     def __init__(self, models):
         # Save models
         self.models = models
@@ -19,21 +28,33 @@ class Interpreter(object):
         # Determine type of command
         if operator == 'create':
             if target == 'connection':
-                if len(words) > 2:
-                    title = ' '.join(words[2:])
+                title = ' '.join(words[2:])
+                if Interpreter.validate_connection(title):
                     kwargs = {
                         'source_id': self.models['area'].id,
                         'title': title,
                     }
                     return reverse('explore:new_connection', kwargs=kwargs)
+                else:
+                    activity = Activity(
+                        area=self.models['area'],
+                        creator=self.models['user'],
+                        creator_only=True,
+                        activity_text='Connections must be one of: ' + ', '.join(Interpreter.ALLOWED_CONNECTIONS) + '.',
+                    )
+                    activity.save()
+                    return reverse('explore:area', args=[self.models['area'].id])
         elif operator == 'edit':
             if target == 'description':
                 return reverse('explore:area_description', args=[self.models['area'].id])
+        else:
+            # If no command, leave a message
+            activity_text = f'{self.models["user"].username}: {command}'
+            activity = Activity(
+                creator=self.models['user'],
+                area=self.models['area'],
+                activity_text=activity_text)
+            activity.save()
 
-        # If no command, leave a message
-        activity_text = f'{self.models["user"].username}: {command}'
-        activity = Activity(
-            creator=self.models['user'],
-            area=self.models['area'],
-            activity_text=activity_text)
-        activity.save()
+    def validate_connection(title):
+        return len(title) > 0 and title in Interpreter.ALLOWED_CONNECTIONS
