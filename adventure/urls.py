77b559bfdeb3658878e2c7a274d5745b5f339b04
Edward L. Platt <ed@elplatt.com>
2020-02-09 01:38:15 +0000

    Add redirect from root url.

--- adventure/urls.py
+++ adventure/urls.py
@@ -18,6 +18,7 @@ import os
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from django.views.static import serve
 
 import home.views
@@ -26,6 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='explore/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     path('explore/', include('explore.urls')), 
     path('accounts/register', home.views.register, name='register'),
