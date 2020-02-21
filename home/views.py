9a941645fdfee948c7f096812b6a767017629829
Edward L Platt <ed@elplatt.com>
2020-02-21 14:39:52 -0500

    Add registration to dash and add next functionality to registration.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -13,7 +15,8 @@ def register(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('explore:index')
+            url = request.POST.get('next', reverse('explore:index'))
+            return HttpResponseRedirect(url)
     else:
         form = UserCreationForm(label_suffix='')
     return render(request, 'registration/register.html', {'form': form})
